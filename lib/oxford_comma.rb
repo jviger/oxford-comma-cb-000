f2c412af3743c7902855c98ced113bb1cf6a5e86
Jason Viger <jason.viger@gmail.com>
2018-08-13 11:14:30 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,11 @@ def oxford_comma(array)
  
  if array.length == 1 
    return array[0]
-else 
+elsif array.length == 2
+
+return array[0] << " and " << array[1]
+
+else
   x = 0
     while x < array.length - 1 do
   
